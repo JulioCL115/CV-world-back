368e7c8ec305055450337b282638ccd91a383cac
Matias <SaliskyMatias@gmail.com>
2024-02-05 17:19:08 -0300

    Arreglando errores y creando el handler para crear cv

--- server/src/controllers/cvController/postCvController.js
+++ server/src/controllers/cvController/postCvController.js
@@ -1,5 +1,19 @@
-const postCvController = () => {
+const { Cv, User, Comment } = require('../../db');
 
+const postCvController = async (name, image, description, experience, contact, study) => {
+
+    const [newCv, created] = await Cv.findOrCreate({
+        where: {
+            name,
+            image,
+            description,
+            experience,
+            contact,
+            study
+        }
+    });
+
+    return newCv;
 }
 
 module.exports = postCvController;
--- server/src/handlers/cvHandler/createCv.js
+++ server/src/handlers/cvHandler/createCv.js
@@ -1,7 +1,14 @@
 const postCvController = require('../../controllers/cvController/postCvController');
 
 const createCv = async (req, res) => {
+    const { name, image, description, experience, contact, study } = req.body;
 
+    try {
+        const cvCreated =  await postCvController(name, image, description, experience, contact, study);
+        res.status(201).json(cvCreated);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 };
 
 module.exports = createCv;
