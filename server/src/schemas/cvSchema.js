d6c58ed8697d0a62606f1270e984b0456127f6e4
Matias <SaliskyMatias@gmail.com>
2024-02-06 23:16:04 -0300

    Modificando modelo cv y agregando relacion user-comment

--- server/src/schemas/cvSchema.js
+++ server/src/schemas/cvSchema.js
@@ -2,7 +2,9 @@ const { z, ZodError } = require('zod');
 
 const createCvSchema = z.object({
     name: z.string().nonempty('Name is required').min(3, 'Name is too short').max(20, 'Name is too long'),
-    image: z.string().nonempty('Image is required'),
+    image: z.array(z.unknown()).refine(arr => arr && arr.length >  0, {
+        message: 'Image is required'
+    }),
     description: z.string().nonempty('Description is required').min(10, 'Description is too short').max(50, 'Description is too long'),
     experience: z.array(z.unknown()).refine(arr => arr && arr.length >  0, {
         message: 'Experience is required'
