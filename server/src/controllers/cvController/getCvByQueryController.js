5af7aaffbad9696940dc697f9968a29676fe3961
Jaime gallego <104177104+JaimeG3@users.noreply.github.com>
2024-02-06 22:54:33 -0500

    update filters

--- server/src/controllers/cvController/getCvByQueryController.js
+++ server/src/controllers/cvController/getCvByQueryController.js
@@ -1,5 +1,23 @@
-const getCvByQueryController = () => {
-
-}
-
-module.exports = getCvByQueryController;
+const getCvByQueryController = (data, filters) => {
+    let response = [...data];
+  
+    if (filters.experienceTitle) {
+      response = response.filter((cv) =>
+        cv.experience.some(
+          (exp) =>
+            exp.title &&
+            exp.title.toLowerCase().includes(filters.experienceTitle.toLowerCase())
+        )
+      );
+    }
+  
+    if (filters.experienceYears) {
+      response.sort((a, b) =>
+        filters.experienceYears === '>5' ? a.price - b.price : b.price - a.price
+      );
+    }
+  
+    return response;
+  };
+  
+  module.exports = getCvByQueryController;
--- server/src/handlers/cvHandler/getAllCvs.js
+++ server/src/handlers/cvHandler/getAllCvs.js
@@ -3,7 +3,21 @@ const getCvByQueryController = require('../../controllers/cvController/getCvByQu
 
 const getAllCvs = async (req, res) => {
 try{
+    const { experienceTitle , experienceYears , studyName, apply } = req.query;
     let response = await getAllCvsController();
+
+    if(experienceTitle || experienceYears || studyName || apply){
+      result = getCvByQueryController(response , {
+        experienceTitle,
+        experienceYears,
+        studyName,
+        apply,
+    } )
+    response = result
+    console.log(result)
+    }
+   
+
     res.status(201).json(response);
 
 }catch (error){
