5a741f512b8ea1acc4858e2018f84f9069bb6153
Matias <SaliskyMatias@gmail.com>
2024-02-08 11:14:46 -0300

    Agregando paginado

--- server/src/controllers/cvController/getAllCvsController.js
+++ server/src/controllers/cvController/getAllCvsController.js
@@ -1,13 +1,19 @@
-const { Cv, User  } = require('../../db');
+const { Cv } = require('../../db');
 
-const getAllCvsController = async () => {
+const getAllCvsController = async (page = 1, pageSize = 6) => {
     try {
-        const allCvsFound = await Cv.findAll({});
+        const offset = (page - 1) * pageSize;
 
-        return allCvsFound;
+        // Realiza la consulta a la base de datos para obtener los CVs paginados
+        const allCvsFound = await Cv.findAndCountAll({
+            limit: pageSize,
+            offset: offset,
+        });
+
+        return allCvsFound.rows;
         
     } catch (error) {
-  
+        throw new Error(`Error al obtener todos los CVs: ${error.message}`);
     }
 
 }
