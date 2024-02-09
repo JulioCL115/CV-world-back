a319fbb86be37f1dd9dc431b6d8f09628d1375fa
Matias <SaliskyMatias@gmail.com>
2024-02-08 21:22:55 -0300

    Agregando rutas lenguage, subscription y category

---
+++ server/src/handlers/categoryHandler/getAllCategories.js
@@ -0,0 +1,20 @@
+const getAllCategoriesController = require('../../controllers/categoryController/getAllCategoriesController');
+
+const getAllCategories = async (req, res) => {
+    try {
+
+        const allCategories = await getAllCategoriesController();
+
+        if(!allCategories || allCategories.length === 0) {
+            return res.status(404).json({ error: "No Categories found." });
+
+        }
+
+        res.status(201).json(allCategories);
+    } catch (error) {
+
+        res.status(500).json({ error: error.message });
+    }
+};
+
+module.exports = getAllCategories;
