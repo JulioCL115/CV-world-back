b0c1f5fb86b4035f82c04e2a5af4683780122624
Matias <SaliskyMatias@gmail.com>
2024-02-13 19:25:14 -0300

    update

--- server/src/routes/subscriptionRouter.js
+++ server/src/routes/subscriptionRouter.js
@@ -2,9 +2,10 @@ const { Router } = require("express");
 const subscriptionRouter = Router();
 const getAllSubscriptions = require('../handlers/subscriptionHandler/getAllSubscriptions');
 const createSubscription = require('../handlers/subscriptionHandler/createSubscription');
+const verifyToken = require('../middlewares/verifyToken');
 
 subscriptionRouter.get('/', getAllSubscriptions);
 
-subscriptionRouter.post('/:userId', createSubscription);
+subscriptionRouter.post('/:userId', verifyToken, createSubscription);
 
 module.exports = subscriptionRouter;
