187ee119fc0dabd6e1dfb3b8e54af58d22550b2f
Leodan Condori <67523914+LeodanCondori7000@users.noreply.github.com>
2024-02-12 20:55:58 -0500

    data

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -6,6 +6,7 @@ const commentRouter = require("./commentRouter");
 const categoryRouter = require('./categoryRouter');
 const languageRouter = require('./languageRouter');
 const subscriptionRouter = require('./subscriptionRouter');
+const cloudinaryRouter = require("./cloudinaryRouter");
 
 router.use('/cv', cvRouter);
 
@@ -18,6 +19,8 @@ router.use('/category', categoryRouter);
 router.use('/language', languageRouter);
 
 router.use('/subscription', subscriptionRouter);
+
+router.use("/cloudinary", cloudinaryRouter);
 
 
 module.exports = router;
