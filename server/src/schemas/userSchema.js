df5faa1958d4f8bcec15ad0fa3526b0884e1b064
Matias <SaliskyMatias@gmail.com>
2024-02-14 12:13:55 -0300

    Arreglando bugs

--- server/src/schemas/userSchema.js
+++ server/src/schemas/userSchema.js
@@ -3,12 +3,12 @@ const joi = require('@hapi/joi');
 const registerUserSchema = joi.object({
     name: joi.string().min(1).max(50).required(),
     email: joi.string().min(1).max(50).required().email(),
-    password: joi.string().min(6).max(20)
+    password: joi.string().min(6).max(100).required()
 });
 
 const loginUserSchema = joi.object({
     email: joi.string().min(1).max(50).required().email(),
-    password: joi.string().min(6).max(20)
+    password: joi.string().min(6).max(100).required()
 });
 
 module.exports = {
