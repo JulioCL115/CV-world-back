b8ae442c29a9134de67490cf3ccd89d6a60c57e4
JulioCL115 <45725622+JulioCL115@users.noreply.github.com>
2024-02-20 11:43:19 -0300

    Update server.js para arrreglar problema de permisos

--- server/src/server.js
+++ server/src/server.js
@@ -2,13 +2,21 @@ const express = require('express');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const routes = require('./routes/index');
 const fileUpload = require("express-fileupload");
-const path = require('path');
+
 const server = express();
 
 server.use(morgan('dev'));
+
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // actualiza para que coincida con el dominio desde el cual realizarás la solicitud, en el segundo campo va la web de la que se realiza la solicitud
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 server.use(express.json());
 server.use(cookieParser());
 server.use(cors());
@@ -16,7 +24,6 @@ server.use(fileUpload({
     useTempFiles: true, 
     tempFileDir: "./uploads", 
 }));
-server.use(express.static(path.resolve('src/public')));
 
 server.use('/', routes);
 
