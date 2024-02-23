bf3c6ead36004a6d9a999c65f819bb7608217e2e
Matias <SaliskyMatias@gmail.com>
2024-02-23 09:59:20 -0300

    terminar rutas comment

---
+++ server/src/handlers/commentHandler/getAllComments.js
@@ -0,0 +1,17 @@
+const getAllCommentsController = require('../../controllers/commentController/getAllCommentsController');
+
+const getAllComments = async (req, res) => {
+    try {
+        const allComments = await getAllCommentsController();
+
+        if(!allComments || allComments.length === 0) {
+            return res.status(404).json({ error: "No Comments found." });
+        }
+
+        res.status(200).json(allComments);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+module.exports = getAllComments;
