f11f4e425258957b491b5ac2248cf04444326e88
Jaime gallego <104177104+JaimeG3@users.noreply.github.com>
2024-02-26 22:24:09 -0500

    up

--- server/src/controllers/subscriptionController/createSubscriptionController.js
+++ server/src/controllers/subscriptionController/createSubscriptionController.js
@@ -3,15 +3,15 @@ const { Subscription } = require("../../db");
 const createSubscriptionController = async ({name, price, included, notIncluded}) => {
     console.log(name,price, included, notIncluded)
     try {
-        // const subscriptionFound = await Subscription.findOne(
-        //     { where: { name, deleted: false } }
-        // );
+         const subscriptionFound = await Subscription.findOne(
+             { where: { name, deleted: false } }
+         );
 
-        // if (subscriptionFound) {
-        //     const error = new Error('Subscription already exists');
-        //     error.statusCode = 409; 
-        //     throw error;
-        // }
+         if (subscriptionFound) {
+             const error = new Error('Subscription already exists');
+             error.statusCode = 409; 
+             throw error;
+         }
 
         const subscriptionCreated = await Subscription.create({
             name,
